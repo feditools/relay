4e3d1b3a6163ef04968cf2a30ca7c214e973ff2f
Tyr Mactire <tyr@pettingzoo.co>
2022-04-26 15:53:20 -0700

    add http server

--- cmd/relay/action/server/start.go
+++ cmd/relay/action/server/start.go
@@ -2,7 +2,9 @@ package server
 
 import (
 	"context"
+	"fmt"
 	"github.com/feditools/relay/cmd/relay/action"
+	"github.com/feditools/relay/internal/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -12,7 +14,14 @@ import (
 var Start action.Action = func(ctx context.Context) error {
 	l := logger.WithField("func", "Start")
 
-	l.Infof("starting")
+	l.Info("starting")
+
+	l.Debug("creating http server")
+	server, err := http.NewServer(ctx)
+	if err != nil {
+		l.Errorf("http server: %s", err.Error())
+		return err
+	}
 
 	// ** start application **
 	errChan := make(chan error)
@@ -21,6 +30,17 @@ var Start action.Action = func(ctx context.Context) error {
 	stopSigChan := make(chan os.Signal)
 	signal.Notify(stopSigChan, syscall.SIGINT, syscall.SIGTERM)
 
+	// start webserver
+
+	go func(s *http.Server, errChan chan error) {
+		l.Debug("starting http server")
+		err := s.Start()
+		if err != nil {
+			errChan <- fmt.Errorf("http server: %s", err.Error())
+		}
+	}(server, errChan)
+
+	// wait for event
 	select {
 	case sig := <-stopSigChan:
 		l.Infof("got sig: %s", sig)
