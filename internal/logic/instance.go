60890cfed119ff8ca35e4e94fd710c852b0a423c
Tyr Mactire <tyr@pettingzoo.co>
2022-04-27 12:29:58 -0700

    simple cache

--- internal/logic/instance.go
+++ internal/logic/instance.go
@@ -1,11 +1,21 @@
 package logic
 
-import "context"
+import (
+	"context"
+	"time"
+)
 
 // GetPeers returns true if a domain matches a block in the database
 func (l *Logic) GetPeers(ctx context.Context) (*[]string, error) {
 	log := logger.WithField("func", "GetPeers")
 
+	// check cache
+	peers, valid := l.getCachedPeerList()
+	if valid {
+		return peers, nil
+	}
+
+	// read from db
 	instances, err := l.db.ReadInstancesWhereFollowing(ctx)
 	if err != nil {
 		log.Errorf("db read: %s", err.Error())
@@ -14,10 +24,32 @@ func (l *Logic) GetPeers(ctx context.Context) (*[]string, error) {
 	log.Debugf("got %d instances", len(instances))
 
 	// populate peer list
-	peers := make([]string, len(instances))
+	newPeers := make([]string, len(instances))
 	for i, instance := range instances {
-		peers[i] = instance.Domain
+		newPeers[i] = instance.Domain
 	}
 
-	return &peers, nil
+	// update cache
+	l.setCachedPeerList(&newPeers)
+
+	return &newPeers, nil
+}
+
+// peer list "cache" functions
+func (l *Logic) getCachedPeerList() (*[]string, bool) {
+	l.cPeerListLock.RLock()
+	if l.cPeerListExpires.Before(time.Now()) {
+		l.cPeerListLock.RUnlock()
+		return nil, false
+	}
+	count := l.cPeerList
+	l.cPeerListLock.RUnlock()
+	return count, true
+}
+
+func (l *Logic) setCachedPeerList(peers *[]string) {
+	l.cPeerListLock.Lock()
+	l.cPeerList = peers
+	l.cPeerListExpires = time.Now().Add(l.cPeerListValidity)
+	l.cPeerListLock.Unlock()
 }
