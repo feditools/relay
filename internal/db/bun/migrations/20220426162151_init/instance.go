eb6f302484d621803cabed4db1bcee55d1126e5f
Tyr Mactire <tyr@pettingzoo.co>
2022-04-26 17:18:44 -0700

    encyrpt private key in db

--- internal/db/bun/migrations/20220426162151_init/instance.go
+++ internal/db/bun/migrations/20220426162151_init/instance.go
@@ -7,11 +7,11 @@ import (
 
 // Instance represents a federated social instance
 type Instance struct {
-	ID         int64           `validate:"required" bun:",pk,nullzero,notnull,unique"`
-	CreatedAt  time.Time       `validate:"-" bun:",nullzero,notnull,default:current_timestamp"`
-	UpdatedAt  time.Time       `validate:"-" bun:",nullzero,notnull,default:current_timestamp"`
-	Domain     string          `validate:"required,fqdn" bun:",nullzero,notnull,unique"`
-	PrivateKey *rsa.PrivateKey `validate:"required_without=InstanceID"`
-	PublicKey  *rsa.PublicKey  `validate:"required"`
-	InboxIRI   string          `validate:"required,url" bun:",nullzero,notnull,unique"`
+	ID         int64          `validate:"required" bun:",pk,nullzero,notnull,unique"`
+	CreatedAt  time.Time      `validate:"-" bun:",nullzero,notnull,default:current_timestamp"`
+	UpdatedAt  time.Time      `validate:"-" bun:",nullzero,notnull,default:current_timestamp"`
+	Domain     string         `validate:"required,fqdn" bun:",nullzero,notnull,unique"`
+	PublicKey  *rsa.PublicKey `validate:"-"`
+	PrivateKey []byte         `validate:"-"`
+	InboxIRI   string         `validate:"required,url" bun:",nullzero,notnull,unique"`
 }
--- internal/models/instance.go
+++ internal/models/instance.go
@@ -3,42 +3,74 @@ package models
 import (
 	"context"
 	"crypto/rsa"
+	"crypto/x509"
 	"github.com/uptrace/bun"
 	"time"
 )
 
 // Instance represents a federated social instance
 type Instance struct {
-	ID         int64           `validate:"required" bun:",pk,nullzero,notnull,unique"`
-	CreatedAt  time.Time       `validate:"-" bun:",nullzero,notnull,default:current_timestamp"`
-	UpdatedAt  time.Time       `validate:"-" bun:",nullzero,notnull,default:current_timestamp"`
-	Domain     string          `validate:"required,fqdn" bun:",nullzero,notnull,unique"`
-	PrivateKey *rsa.PrivateKey `validate:"-"`
-	PublicKey  *rsa.PublicKey  `validate:"-"`
-	InboxIRI   string          `validate:"required,url" bun:",nullzero,notnull,unique"`
+	ID         int64          `validate:"required" bun:",pk,nullzero,notnull,unique"`
+	CreatedAt  time.Time      `validate:"-" bun:",nullzero,notnull,default:current_timestamp"`
+	UpdatedAt  time.Time      `validate:"-" bun:",nullzero,notnull,default:current_timestamp"`
+	Domain     string         `validate:"required,fqdn" bun:",nullzero,notnull,unique"`
+	PublicKey  *rsa.PublicKey `validate:"-"`
+	PrivateKey []byte         `validate:"-"`
+	InboxIRI   string         `validate:"required,url" bun:",nullzero,notnull,unique"`
 }
 
 var _ bun.BeforeAppendModelHook = (*Instance)(nil)
 
 // BeforeAppendModel runs before a bun append operation
-func (f *Instance) BeforeAppendModel(_ context.Context, query bun.Query) error {
+func (i *Instance) BeforeAppendModel(_ context.Context, query bun.Query) error {
 	switch query.(type) {
 	case *bun.InsertQuery:
 		now := time.Now()
-		f.CreatedAt = now
-		f.UpdatedAt = now
+		i.CreatedAt = now
+		i.UpdatedAt = now
 
-		err := validate.Struct(f)
+		err := validate.Struct(i)
 		if err != nil {
 			return err
 		}
 	case *bun.UpdateQuery:
-		f.UpdatedAt = time.Now()
+		i.UpdatedAt = time.Now()
 
-		err := validate.Struct(f)
+		err := validate.Struct(i)
 		if err != nil {
 			return err
 		}
 	}
+	return nil
+}
+
+// GetPrivateKey returns unencrypted private key
+func (i *Instance) GetPrivateKey() (*rsa.PrivateKey, error) {
+	// decrypt bytes
+	b, err := decrypt(i.PrivateKey)
+
+	// convert to private key
+	pk, err := x509.ParsePKCS1PrivateKey(b)
+	if err != nil {
+		return nil, err
+	}
+
+	// return private key
+	return pk, err
+}
+
+// SetPrivateKey sets encrypted private key
+func (i *Instance) SetPrivateKey(pk *rsa.PrivateKey) error {
+	// convert to bytes
+	b := x509.MarshalPKCS1PrivateKey(pk)
+
+	// encrypt bytes
+	data, err := encrypt(b)
+	if err != nil {
+		return err
+	}
+
+	// store bytes
+	i.PrivateKey = data
 	return nil
 }
